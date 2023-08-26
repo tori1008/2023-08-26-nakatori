8ca1ad7ed2e29eba24d706f69d08e32f4578c252
tori1008 <139036961+tori1008@users.noreply.github.com>
2023-08-26 13:27:35 +0900

    Add files via upload

---
+++ php/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/sanitize.css">
+    <link rel="stylesheet" href="css/common.css">
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<header class="header">
+    <div class="header__inner">
+        <a class="header__logo" href="/php03/index.php">
+            World Clock
+        </a>
+    </div>
+</header>
+
+<body>
+
+</body>
+
+</html>
